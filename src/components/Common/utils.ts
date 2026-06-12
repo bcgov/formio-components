072076ccbbf3de02098ef6cfbff2484fe35a77f0
BrandonSharratt <brandon@highwaythreesolutions.com>
2026-06-12 14:32:24 -0700

    update

--- src/components/Common/utils.ts
+++ src/components/Common/utils.ts
@@ -1,5 +1,66 @@
+import _ from 'lodash';
 import Evaluator from './Evaluator';
 const EditFormUtils = {
+  sortAndFilterComponents(components) {
+    return _.filter(_.sortBy(components, 'weight'), (item) => !item.ignore);
+  },
+  unifyComponents(objValue, srcValue) {
+    if (objValue.key && srcValue.key) {
+      if (objValue.skipMerge || srcValue.skipMerge) {
+        return false;
+      }
+      if (objValue.key === srcValue.key) {
+        // Create complete objects by including missing keys.
+        _.each(objValue, (value, prop) => {
+          if (objValue.overrideEditForm || !srcValue.hasOwnProperty(prop)) {
+            srcValue[prop] = value;
+          }
+        });
+        _.each(srcValue, (value, prop) => {
+          if (srcValue.overrideEditForm || !objValue.hasOwnProperty(prop)) {
+            objValue[prop] = value;
+          }
+        });
+
+        if (objValue.components) {
+          srcValue.components = EditFormUtils.sortAndFilterComponents(
+            _.unionWith(objValue.components, srcValue.components, EditFormUtils.unifyComponents)
+          );
+        }
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    return _.isEqual(objValue, srcValue);
+  },
+  logicVariablesTable(additional) {
+    additional = additional || '';
+    return {
+      type: 'htmlelement',
+      tag: 'div',
+      /* eslint-disable prefer-template */
+      content: '<p>The following variables are available in all scripts.</p>' +
+      '<table class="table table-bordered table-condensed table-striped">' +
+      additional +
+      '<tr><th>token</th><td>The decoded JWT token for the authenticated user.</td></tr>' +
+      '<tr><th>user</th><td>The current logged in user</td></tr>' +
+      '<tr><th>form</th><td>The complete form JSON object</td></tr>' +
+      '<tr><th>submission</th><td>The complete submission object.</td></tr>' +
+      '<tr><th>data</th><td>The complete submission data object.</td></tr>' +
+      '<tr><th>row</th><td>Contextual "row" data, used within DataGrid, EditGrid, and Container components</td></tr>' +
+      '<tr><th>component</th><td>The current component JSON</td></tr>' +
+      '<tr><th>instance</th><td>The current component instance.</td></tr>' +
+      '<tr><th>value</th><td>The current value of the component.</td></tr>' +
+      '<tr><th>moment</th><td>The moment.js library for date manipulation.</td></tr>' +
+      '<tr><th>_</th><td>An instance of <a href="https://lodash.com/docs/" target="_blank">Lodash</a>.</td></tr>' +
+      '<tr><th>utils</th><td>An instance of the <a href="http://formio.github.io/formio.js/docs/identifiers.html#utils" target="_blank">FormioUtils</a> object.</td></tr>' +
+      '<tr><th>util</th><td>An alias for "utils".</td></tr>' +
+      '</table><br/>'
+      /* eslint-enable prefer-template */
+    };
+  },
   javaScriptValue(title, property, propertyJSON, weight, exampleHTML, exampleJSON, additionalParams = '', excludeJSONLogic) {
     const components = [
       this.logicVariablesTable(additionalParams),
